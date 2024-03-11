e00756732e0a101fb8216ae33d337d662f290fa6
Max <91115390+maxbakhub@users.noreply.github.com>
2024-03-11 14:00:20 +0500

    Create ImportRDGateway_Cert_From_IIS.ps1

---
+++ RDS/ImportRDGateway_Cert_From_IIS.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+Imports a cert from WACS renewal into the RD Gateway SSL binding
+.DESCRIPTION
+Note that this script is intended to be run via the install script plugin from win-acme via the batch script wrapper. As such, we use positional parameters to avoid issues with using a dash in the cmd line. 
+Proper information should be available here
+https://github.com/PKISharp/win-acme/wiki/Example-Scripts
+https://woshub.com/howto-install-free-lets-encrypt-ssl-cert-on-iis/
+#>
+
+Import-Module RemoteDesktopServices
+Import-Module WebAdministration
+
+$NewCertThumbprint = (Get-ChildItem IIS:SSLBindings)[0].Thumbprint
+
+$CertInStore = Get-ChildItem -Path Cert:\LocalMachine -Recurse | Where-Object {$_.thumbprint -eq $NewCertThumbprint} | Sort-Object -Descending | Select-Object -f 1
+if($CertInStore){
+    try{
+        # Cert must exist in the personal store of machine to bind to RD Gateway
+        if($CertInStore.PSPath -notlike "*LocalMachine\My\*"){
+            $SourceStoreScope = 'LocalMachine'
+            $SourceStorename = $CertInStore.PSParentPath.split("\")[-1]
+
+            $SourceStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $SourceStorename, $SourceStoreScope
+            $SourceStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+            
+            $cert = $SourceStore.Certificates | Where-Object {$_.thumbprint -eq $CertInStore.Thumbprint}
+            
+            
+            
+            $DestStoreScope = 'LocalMachine'
+            $DestStoreName = 'My'
+            
+            $DestStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $DestStoreName, $DestStoreScope
+            $DestStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+            $DestStore.Add($cert)
+            
+            
+            $SourceStore.Close()
+            $DestStore.Close()
+
+            $CertInStore = Get-ChildItem -Path Cert:\LocalMachine\My -Recurse | Where-Object {$_.thumbprint -eq $NewCertThumbprint} | Sort-Object -Descending | Select-Object -f 1
+        }
+        Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $CertInStore.Thumbprint -ErrorAction Stop
+        Restart-Service TSGateway -Force -ErrorAction Stop
+        "Cert thumbprint set to RD Gateway listener and service restarted"
+    }catch{
+        "Cert thumbprint was not set successfully"
+        "Error: $($Error[0])"
+    }
+}else{
+    "Cert thumbprint not found in the cert store... which is strange because it should be there."
+}
